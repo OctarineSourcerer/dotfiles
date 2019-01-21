1b04aaea12c6582979c92b3b94421977984ca801
Dan Gomme <daniel.gomme@yahoo.co.uk>
2019-01-21 13:22:05 +0000

    Compton additions! Probably zathura and a couple others too

--- dotfiles/common/.config/fish/config.fish
+++ dotfiles/common/.config/fish/config.fish
@@ -8,3 +8,6 @@ set -xU LESS_TERMCAP_se (printf "\e[0m")
 set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
+# Sets up link to mDrive, must have approved ssh key and mDrive folder in home
+alias mDrive="sshfs -o IdentityFile=~/.ssh/id_rsa dsdgom@unix4.essex.ac.uk:/ufs/servh02/users/dsdgom ~/mDrive/"
