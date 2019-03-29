a27282f81bd27e3dfd8183aeb91fe76671f7f06c
Dan <daniel.gomme@yahoo.co.uk>
2019-03-29 18:20:45 +0000

    Right capitalisation for IntelliJ fix

--- dotfiles/common/.config/fish/config.fish
+++ dotfiles/common/.config/fish/config.fish
@@ -10,7 +10,7 @@ set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
 # Necessary for IntelliJ IDEA to start up nicely
-set -xU _JaVA_AWT_WM_NONREPARENTING 1
+set -xU _JAVA_AWT_WM_NONREPARENTING 1
 
 # Sets up link to mDrive, must have approved ssh key and mDrive folder in home
 alias mDrive="sshfs -o IdentityFile=~/.ssh/id_rsa dsdgom@unix4.essex.ac.uk:/ufs/servh02/users/dsdgom ~/mDrive/"
