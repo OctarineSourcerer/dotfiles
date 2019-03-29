6326f1cc959234b4c8af056f9c90acc43035580f
Dan Gomme <daniel.gomme@yahoo.co.uk>
2019-03-29 17:47:39 +0000

    IntelliJ-fixing env variable, alternate automatic scheme

--- dotfiles/common/.config/fish/config.fish
+++ dotfiles/common/.config/fish/config.fish
@@ -9,5 +9,8 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
+# Necessary for IntelliJ IDEA to start up nicely
+set -xU _JaVA_AWT_WM_NONREPARENTING 1
+
 # Sets up link to mDrive, must have approved ssh key and mDrive folder in home
 alias mDrive="sshfs -o IdentityFile=~/.ssh/id_rsa dsdgom@unix4.essex.ac.uk:/ufs/servh02/users/dsdgom ~/mDrive/"
