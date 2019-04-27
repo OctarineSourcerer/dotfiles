4240cb33f593217f885936a075f2020deedd4062
Dan Gomme <daniel.gomme@yahoo.co.uk>
2019-04-27 13:35:01 +0100

    Added nvim as EDITOR and VISUAL for fish

--- dotfiles/common/.config/fish/config.fish
+++ dotfiles/common/.config/fish/config.fish
@@ -9,8 +9,14 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
+set -xU VISUAL "nvim"
+set -xU EDITOR "nvim"
+
 # Necessary for IntelliJ IDEA to start up nicely
 set -xU _JAVA_AWT_WM_NONREPARENTING 1
 
+set -xU VK_ICD_FILENAMES /usr/share/vulkan/icd.d/nvidia_icd.json
+
 # Sets up link to mDrive, must have approved ssh key and mDrive folder in home
 alias mDrive="sshfs -o IdentityFile=~/.ssh/id_rsa dsdgom@unix4.essex.ac.uk:/ufs/servh02/users/dsdgom ~/mDrive/"
+alias vim="nvim"
